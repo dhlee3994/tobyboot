9b86c1c5ea9718013245b5e7e0c3a55d99c36b99
dhlee3994 <dhlee3994@gmail.com>
2023-01-20 14:44:55 +0900

    톰캣 서버를 직접 띄워서 서블릿 등록

--- src/main/java/dev/dhlee/tobyboot/TobyBootApplication.java
+++ src/main/java/dev/dhlee/tobyboot/TobyBootApplication.java
@@ -1,13 +1,30 @@
 package dev.dhlee.tobyboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TobyBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyBootApplication.class, args);
-    }
+        final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().print("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
 
+    }
 }
