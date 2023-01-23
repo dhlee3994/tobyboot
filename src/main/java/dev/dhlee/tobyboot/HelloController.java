2917788443da54907e50888299300a44d4588487
dhlee3994 <dhlee3994@gmail.com>
2023-01-23 10:05:32 +0900

    스프링에서 제공하는 어노테이션으로 교체

--- src/main/java/dev/dhlee/tobyboot/HelloController.java
+++ src/main/java/dev/dhlee/tobyboot/HelloController.java
@@ -1,13 +1,11 @@
 package dev.dhlee.tobyboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping
-@MyComponent
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -15,7 +13,6 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @ResponseBody
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
