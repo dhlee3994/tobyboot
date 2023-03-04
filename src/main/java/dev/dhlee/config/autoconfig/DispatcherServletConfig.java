e18f1482f16d159245d79b411230725a8543bd31
dhlee3994 <dhlee3994@gmail.com>
2023-03-04 10:31:20 +0900

    자동 구성 어노테이션 적용

--- src/main/java/dev/dhlee/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/dev/dhlee/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package dev.dhlee.config.autoconfig;
 
+import dev.dhlee.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
