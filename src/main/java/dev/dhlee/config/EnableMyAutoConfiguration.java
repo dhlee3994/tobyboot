d9551576f897c0512d5e6ed8dd4af3028092dede
dhlee3994 <dhlee3994@gmail.com>
2023-03-04 10:14:04 +0900

    인프라 빈 구성 정보 분리

---
+++ src/main/java/dev/dhlee/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,16 @@
+package dev.dhlee.config;
+
+import dev.dhlee.config.autoconfig.DispatcherServletConfig;
+import dev.dhlee.config.autoconfig.TomcatWebServerConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfiguration {
+}
--- src/main/java/dev/dhlee/tobyboot/TobyBootApplication.java
+++ src/main/java/dev/dhlee/tobyboot/TobyBootApplication.java
@@ -1,8 +1,9 @@
 package dev.dhlee.tobyboot;
 
+import dev.dhlee.config.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class TobyBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(TobyBootApplication.class, args);
