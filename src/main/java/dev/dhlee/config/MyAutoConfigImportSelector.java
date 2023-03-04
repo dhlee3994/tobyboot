f17e48177462169354ae0c24dd00f11b8aebdadf
dhlee3994 <dhlee3994@gmail.com>
2023-03-04 10:28:07 +0900

    자동 구성 정보 파일 분리

--- src/main/java/dev/dhlee/config/MyAutoConfigImportSelector.java
+++ src/main/java/dev/dhlee/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,24 @@
 package dev.dhlee.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "dev.dhlee.config.autoconfig.DispatcherServletConfig",
-                "dev.dhlee.config.autoconfig.TomcatWebServerConfig"
-        };
+        final Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+
+        return StreamSupport.stream(candidates.spliterator(), false)
+                .toArray(String[]::new);
     }
 }
